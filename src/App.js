38db4aabe4dbe82dfe4c4b04a3ee2cf6dae5d9f1
Sonakshi1901 <sonakshi1901@gmail.com>
2021-05-25 15:32:13 +0400

    added a preloader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+import { useState, useEffect } from "react";
+import { css } from "@emotion/react";
+import HashLoader from "react-spinners/HashLoader";
 import Footer from './components/footer/footer'
 import "./App.css";
 import { Navbar } from "./components/navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/main";
 
+const loadercss = css`
+ display: block;
+ padding-top: 560px;
+ margin-left: 48%;
+ text-align: center;
+`
+
 function App() {
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() =>{
+      setLoading(false);
+    }, 4000)
+  }, [])
+
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="main">
-          <Route path="/" exact component={Main} />
+      loading ?
+        <HashLoader
+         color={"#ffffff"}
+         loading={loading}
+         css={loadercss}
+         size={80}
+         />
+
+      :
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="main">
+            <Route path="/" exact component={Main} />
+          </div>
+        <MessengerCustomerChat
+          pageId="104646555139813"
+          appId="1639085279814437"
+          themeColor="#b20000"
+        />
+        <Footer/>
         </div>
-      <MessengerCustomerChat
-        pageId="104646555139813"
-        appId="1639085279814437"
-        themeColor="#b20000"  
-      />
-      <Footer/>
-      </div>
-    </Router>
+      </Router>
+
   );
 }
 
