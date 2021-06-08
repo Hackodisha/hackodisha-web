a4a469f929518549e283a9c1ffb615e351f127a5
sumana2001 <sumana.basu2001@gmail.com>
2021-06-08 15:49:53 +0530

    404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const NotFound = () =>
-  <div>
-    <h3>404 page not found</h3>
-    <p>We are sorry but the page you are looking for does not exist.</p>
+  <div style={{minHeight:"85vh", alignItems:"center",justifyContent:"center"}}>
+    <h1 style={{fontSize:"25vw",color:"#fff", margin:"0"}}>404</h1>
+    <h3 style={{fontSize:"8vw",color:"#fff",marginTop:"-10%"}}>Page not found!</h3>
   </div>
 
 export default NotFound
