4ed5c7aa99799d9a079006b5df4818327bbf976e
Sonakshi1901 <sonakshi1901@gmail.com>
2021-06-13 16:06:50 +0400

    minor changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import Raisebox from "../components/raisebox/raisebox";
 import "./style.css";
