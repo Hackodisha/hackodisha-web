b319f7607de43ba12965e821a9c8662a5024490b
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-06-14 14:45:07 +0200

    change tags from divs to sections

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -10,43 +10,43 @@ import Schedule from "./schedule";
 import { useEffect } from "react";
 
 function Main() {
-  useEffect(()=>{
-    const script = document.createElement('script');
-    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://apply.devfolio.co/v2/sdk.js";
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
     return () => {
       document.body.removeChild(script);
-    }
-  })
+    };
+  });
   return (
-    <div>
-      <div id="home">
+    <>
+      <section id="home">
         <Home />
-      </div>
-      <div id="about">
+      </section>
+      <section id="about">
         <About />
-      </div>
-      <div id="tracks">
+      </section>
+      <section id="tracks">
         <Tracks />
-      </div>
-      <div id="sponsors">
+      </section>
+      <section id="sponsors">
         <Sponsors />
-      </div>
-      <div id="schedule">
+      </section>
+      <section id="schedule">
         <Schedule />
-      </div>
-      <div id="events">
+      </section>
+      <section id="events">
         <Events />
-      </div>
-      <div id="prizes">
+      </section>
+      <section id="prizes">
         <Prizes />
-      </div>
-      <div id="faq">
+      </section>
+      <section id="faq">
         <Faq />
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
 
