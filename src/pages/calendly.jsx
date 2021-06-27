80098c799d8a24452e00e3616192be48f140b2b3
Sonakshi1901 <sonakshi1901@gmail.com>
2021-06-27 16:21:32 +0400

    minor fix

--- src/pages/calendly.jsx
+++ src/pages/calendly.jsx
@@ -11,7 +11,6 @@ function Calendly() {
         <h1 className="calendly_heading">
           Schedule<span style={{ color: "#ff0000" }}> Meet</span>
        </h1>
-       <h2 className="calendly_h2">Schedule a 30 minutes call with us via Google meet</h2>
        <div className="calendly_box">
           <InlineWidget url="https://calendly.com/hackodisha" />
        </div>
