21b024d3438158e20587fc395c57b8aad0ce7773
Sonakshi1901 <sonakshi1901@gmail.com>
2021-07-09 17:07:19 +0400

    Tracklist

---
+++ src/components/home-cards/TracksData.jsx
@@ -0,0 +1,51 @@
+const TrackData = [
+    {
+        id:1,
+        name: "Health",
+        image:require("../../assets/icons/health.png").default,
+        alt:"health_track"
+        className:"tracks_icon"
+        info: "Health"
+    },
+    {
+        id:2,
+        name:"Education",
+        className:"tracks_icon",
+        image:require("../../assets/icons/education.png").default,
+        alt:"education_track"
+        info:"Education"
+    },
+    {
+        id:3,
+        name:"Environment",
+        image:require("../../assets/icons/environment.png").default,
+        alt:"environment_track"
+        className:"tracks_icon"
+        info: "Environment"
+    },
+    {
+        id:4,
+        name:"Women Safety",
+        image:require("../../assets/icons/security.png").default,
+        alt:"safety_track"
+        className="tracks_icon"
+        info: "Women Safety"
+    },
+    {
+        id:5,
+        name:"Disaster Management",
+        image:require("../../assets/icons/disaster.png").default,
+        alt:"disaster_track"
+        className:"tracks_icon"
+        info: "Disaster"
+    },
+    {
+        id:6,
+        name:"Open Innovation",
+        image:require("../../assets/icons/open.png").default,
+        alt:"open_track"
+        className:"tracks_icon"
+        info: "opens"
+    },
+
+];
