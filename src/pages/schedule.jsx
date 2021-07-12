20cad60426683e5ca01b5cd9a2e4a7cbc31d3384
Sonakshi1901 <sonakshi1901@gmail.com>
2021-07-12 21:15:32 +0400

    Added a Table to the Schedule page

--- src/pages/schedule.jsx
+++ src/pages/schedule.jsx
@@ -1,6 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 import Raisebox from "../components/raisebox/raisebox";
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(Time, Description) {
+  return { Time, Description };
+}
+
+const rows = [
+  createData('9:00 - 10:00', "Introductory session"),
+  createData('9:00 - 10:00', "Introductory session"),
+  createData('9:00 - 10:00', "Introductory session"),
+  createData('9:00 - 10:00', "Introductory session"),
+  createData('9:00 - 10:00', "Introductory session"),
+];
 
 const ScheduleContainer = styled.div`
   height: 100%;
@@ -12,7 +50,6 @@ const ScheduleContainer = styled.div`
 
 const Scheduleh1 = styled.h1`
   font-weight: 900;
-  padding-top: 5%;
   color: #fff;
   font-size: 6rem;
   line-height: 0.8em;
@@ -34,6 +71,7 @@ const Scheduleh2 = styled.h2`
 `;
 
 function Schedule() {
+
   return (
     <Raisebox>
       <ScheduleContainer>
@@ -41,7 +79,27 @@ function Schedule() {
           HACKATHON <span style={{ color: "#ff0000" }}> SCHEDULE</span>
         </Scheduleh1>{" "}
       </ScheduleContainer>
-      <Scheduleh2>To be revealed soon</Scheduleh2>
+      <TableContainer component={Paper} style={{ width: "60vw", marginLeft: "18%", marginTop: "-2%"}}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Time</StyledTableCell>
+              <StyledTableCell>Description</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.Time}>
+                <StyledTableCell component="th" scope="row">
+                  {row.Time}
+                </StyledTableCell>
+                <StyledTableCell>{row.Description}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <p style={{ color: "#18191a"}}>Table</p>
     </Raisebox>
   );
 }
