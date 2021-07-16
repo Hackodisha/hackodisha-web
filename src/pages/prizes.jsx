6b4ddd4e91a7d0d5f0a8c4bdd2b6473372f824f1
Debasish MIshra <76249530+debasish-creator@users.noreply.github.com>
2021-07-16 19:39:00 +0530

    prizes section updates
    minor changes including background color and cursor style updated.

--- src/pages/prizes.jsx
+++ src/pages/prizes.jsx
@@ -1,41 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import Raisebox from "../components/raisebox/raisebox";
 import { useState } from "react";
 import {Col, Row} from 'react-grid-system';
 import "./style.css";
 import Popup from "reactjs-popup";
 
-const PrizesContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-`;
 
-const Prizesh1 = styled.h1`
-  font-weight: 900;
-  padding-top: 5%;
-  color: #fff;
-  font-size: 6rem;
-  line-height: 0.8em;
-  letter-spacing: -0.07em;
-
-  @media screen and (max-width: 576px){
-    font-size: 4rem;
-  }
-`;
-
-const Prizesh2 = styled.h2`
-  text-align: center;
-  color: #fff;
-  font-size: 2.5rem;
-
-  @media screen and (max-width: 576px){
-    font-size: 2rem;
-  }
-`;
 
 
 
