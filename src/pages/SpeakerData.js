2c3beb3be3efea349d6afa80db342fa1cbc57e45
Debasish MIshra <76249530+debasish-creator@users.noreply.github.com>
2021-07-16 16:33:54 +0530

    Speakers page updated. This fixes  #79

---
+++ src/pages/SpeakerData.js
@@ -0,0 +1,71 @@
+const speakerData = [
+    {
+        id:1,
+        className:"logo_speaker border_grad",
+        link:"https://www.portis.io/",
+        image:("https://hackon.tech/_nuxt/img/Juan-Pablo.960133a.png"),
+        alt:"Portis silver sponsor for Hackodisha",
+        style:{width:"300px"},
+        name:'Debasish',
+        description:"Program manager",
+        company_logo:"https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png",
+        alt_company:"Github",
+        
+    },
+    {
+        id:2,
+        className:"logo_speaker border_grad",
+        link:"https://www.portis.io/",
+        image:("https://hackon.tech/_nuxt/img/Juan-Pablo.960133a.png"),
+        alt:"Portis silver sponsor for Hackodisha",
+        style:{width:"300px"},
+        name:'Lorem Ipsum',
+        description:"Program manager",
+        company_logo:"https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png",
+        alt_company:"Github",
+        
+    },
+    {
+        id:3,
+        className:"logo_speaker border_grad",
+        link:"https://www.portis.io/",
+        image:("https://hackon.tech/_nuxt/img/Juan-Pablo.960133a.png"),
+        alt:"Portis silver sponsor for Hackodisha",
+        style:{width:"300px"},
+        name:'Lorem Ipsum',
+        description:"Program manager",
+        company_logo:"https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png",
+        alt_company:"Github",
+        
+    },
+    {
+        id:4,
+        className:"logo_speaker border_grad",
+        link:"https://www.portis.io/",
+        image:("https://hackon.tech/_nuxt/img/Juan-Pablo.960133a.png"),
+        alt:"Portis silver sponsor for Hackodisha",
+        style:{width:"300px"},
+        name:'Lorem Ipsum',
+        description:"Program manager",
+        company_logo:"https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png",
+        alt_company:"Github",
+        
+    },
+    {
+        id:5,
+        className:"logo_speaker border_grad",
+        link:"https://www.portis.io/",
+        image:("https://hackon.tech/_nuxt/img/Juan-Pablo.960133a.png"),
+        alt:"Portis silver sponsor for Hackodisha",
+        style:{width:"300px"},
+        name:'Lorem Ipsum',
+        description:"Program manager",
+        company_logo:"https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png",
+        alt_company:"Github",
+        
+    },
+   
+
+];
+export default speakerData;
+
