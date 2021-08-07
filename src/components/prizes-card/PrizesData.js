bd15db8ca6e1a7ec12f2cb544c4f634ba08af0b6
Debasish MIshra <76249530+debasish-creator@users.noreply.github.com>
2021-08-07 22:51:19 +0530

    prizes section updates

---
+++ src/components/prizes-card/PrizesData.js
@@ -0,0 +1,218 @@
+const prizesData = [
+  {
+    id: 1,
+    description: (
+      <div>
+        Cash Prize* <br />
+        T-shirt <br />
+        Stickers
+        <br />
+        $1000 digital Ocean credits <br />
+        HoverRobotix gift vochure worth ₹20k <br />
+        Custom XYZ domain for 1 year
+        <br />
+        Interview Buddy discount vochures <br />
+        Taskade lifetime access <br />
+        Coding Block Vouchers
+        <br />
+        Replit 90 days trail
+        <br />
+        Script Foundation Academy 90 days premium access
+        <br />
+        1Password access for 1 year <br />
+        12 months Framer subscription
+        <br />
+        Access to Wolfarm for 30 days
+        <br />
+        Qoom 3 months starter plan
+        <br />
+        Showcase on Qoom's gallery
+        <br />
+        Draftbit 40% off for 1 year
+        <br />
+        Balsamic 90 days extended trail
+        <br />
+        Axure access
+        <br />
+        Rosenfeld Media free book giveaways on their site
+        <br />
+        90% off on AI champ courses
+        <br />
+        ebook(40$) benefits from No Starch Press
+        <br />
+        Free Individual licenses to all Jetbrains desktop tools for limited time
+        <br />
+      </div>
+    ),
+  },
+  {
+    id: 2,
+    description: (
+      <div>
+        Cash Prize* <br />
+        T-shirt <br />
+        Stickers
+        <br />
+        $600 digital Ocean credits <br />
+        HoverRobotix gift vochure worth ₹10k <br />
+        Custom XYZ domain for 1 year
+        <br />
+        Interview Buddy discount vochures <br />
+        Taskade lifetime access <br />
+        Coding Block Vouchers
+        <br />
+        Script Foundation Academy 90 days premium access
+        <br />
+        1Password access for 1 year <br />
+        12 months Framer subscription
+        <br />
+        Replit 90 days trail
+        <br />
+        Access to Wolfarm for 30 days
+        <br />
+        Qoom 3 months starter plan
+        <br />
+        Showcase on Qoom's gallery
+        <br />
+        Draftbit 40% off for 1 year
+        <br />
+        Balsamic 90 days extended trail
+        <br />
+        Axure access
+        <br />
+        Rosenfeld Media free book giveaways on their site
+        <br />
+        70% off on AI champ courses
+        <br />
+        ebook(40$) benefits from No Starch Press
+        <br />
+        Free Individual licenses to all Jetbrains desktop tools for limited time
+        <br />
+      </div>
+    ),
+  },
+  {
+    id: 3,
+    description: (
+      <div>
+        Cash Prize* <br />
+        T-shirt <br />
+        Stickers
+        <br />
+        $400 digital Ocean credits <br />
+        Custom XYZ domain for 1 year
+        <br />
+        Interview Buddy discount vochures <br />
+        Taskade lifetime access <br />
+        Replit 90 days trail
+        <br />
+        Coding Block Vouchers
+        <br />
+        Script Foundation Academy 90 days premium access
+        <br />
+        1Password access for 1 year <br />
+        Access to Wolfarm for 30 days
+        <br />
+        Qoom 3 months starter plan
+        <br />
+        Showcase on Qoom's gallery
+        <br />
+        Draftbit 40% off for 1 year
+        <br />
+        Balsamic 90 days extended trail
+        <br />
+        Axure access
+        <br />
+        Rosenfeld Media free book giveaways on their site
+        <br />
+        70% off on AI champ courses
+        <br />
+        ebook(40$) benefits from No Starch Press
+        <br />
+        Free Individual licenses to all Jetbrains desktop tools for limited time
+        <br />
+      </div>
+    ),
+  },
+  {
+    id: 4,
+    description: (
+      <div>
+        T-shirt <br />
+        Stickers
+        <br />
+        Custom XYZ domain for 1 year
+        <br />
+        Interview Buddy discount vochures <br />
+        Taskade lifetime access <br />
+        Coding Block Vouchers
+        <br />
+        Script Foundation Academy 90 days premium access
+        <br />
+        1Password access for 1 year <br />
+        Replit 90 days trail
+        <br />
+        Access to Wolfarm for 30 days
+        <br />
+        Qoom 3 months starter plan
+        <br />
+        Showcase on Qoom's gallery
+        <br />
+        Draftbit 40% off for 1 year
+        <br />
+        Balsamic 90 days extended trail
+        <br />
+        Axure access
+        <br />
+        Rosenfeld Media free book giveaways on their site
+        <br />
+        70% off on AI champ courses
+        <br />
+        ebook(40$) benefits from No Starch Press
+        <br />
+        Free Individual licenses to all Jetbrains desktop tools for limited time
+        <br />
+      </div>
+    ),
+  },
+  {
+    id: 5,
+    description: (
+      <div>
+        Stickers
+        <br />
+        Custom XYZ domain for 1 year
+        <br />
+        Interview Buddy discount vochures <br />
+        Taskade lifetime access <br />
+        Coding Block Vouchers
+        <br />
+        Script Foundation Academy 90 days premium access
+        <br />
+        1Password access for 1 year <br />
+        Access to Wolfarm for 30 days
+        <br />
+        Qoom 3 months starter plan
+        <br />
+        Showcase on Qoom's gallery
+        <br />
+        Draftbit 40% off for 1 year
+        <br />
+        Balsamic 90 days extended trail
+        <br />
+        Axure access
+        <br />
+        Rosenfeld Media free book giveaways on their site
+        <br />
+        70% off on AI champ courses
+        <br />
+        ebook(40$) benefits from No Starch Press
+        <br />
+        Free Individual licenses to all Jetbrains desktop tools for limited time
+        <br />
+      </div>
+    ),
+  },
+];
+
+export default prizesData;
