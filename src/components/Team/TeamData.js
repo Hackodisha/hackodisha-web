b0cbdacf97b42ca0de56ac873b08775fb5e3fb6a
manjeetdhayal <manjeetdhayal786@gmail.com>
2021-09-15 11:26:26 +0530

    Merge branch 'main' of https://github.com/Hackodisha/hackodisha-web

--- src/components/Team/TeamData.js
+++ src/components/Team/TeamData.js
@@ -19,7 +19,7 @@ const TeamData = [
         id: 3, 
         Name: "Manjeet Dhayal",
         Role: "Organiser", 
-        imgPath: "https://images-na.ssl-images-amazon.com/images/I/819gOyz8B4L.png",
+        imgPath: "https://res.cloudinary.com/manjeetdhayal/image/upload/v1631607021/Manjeet/image_3Manjeet_square_w9vr0d.png",
         likedIn: "",
     },
     {
